ef9c858496b156641d0ce0dbbb7543786639235a
Soumya Bodigam <sowmyareddy1911@gmail.com>
2025-02-26 00:06:49 +0530

    Update googleApi.jsx

--- orb/src/utils/googleApi.jsx
+++ orb/src/utils/googleApi.jsx
@@ -2,7 +2,7 @@ import { gapi } from "gapi-script";
 
 const initGoogleClient = () => {
   const CLIENT_ID = "777154732450-4id6v59l38il53qo33fk5fpprerufaqc.apps.googleusercontent.com"; // Replace with your OAuth Client ID
-  const API_KEY = "AIzaSyAq9wZ88N5HC1Jbb-cL0tQ3ozbOEsfqHFk"; // Replace with your API Key
+  const API_KEY = ""; // Replace with your API Key
   const SCOPES = "https://www.googleapis.com/auth/calendar.events";
 
   gapi.load("client:auth2", () => {
